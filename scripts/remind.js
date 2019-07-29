073da03194f5e86153b46d6b32547a048d44b3fb
dwq <dwqmas@gmail.com>
2019-07-29 21:16:32 +0800

    add weather remaind

--- scripts/remind.js
+++ scripts/remind.js
@@ -1,10 +1,41 @@
-const express = require('express');
-const app = express();
- 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+const axios = require('axios');
+const _ = require('lodash');
+const moment = require('moment');
 
-app.listen(3000, () => {
-    console.log('Server are running………………………………');
-});
+async function remind() {
+    const weatherData = await getWeather();
+    const curDataWeather = _.find(weatherData.data, {date: timeString(new Date(), 'YYYY-MM-DD')})
+    await send(curDataWeather);
+    return 0
+  }
+
+  function getWeather() {
+    return axios.get('https://www.tianqiapi.com/api/?version=v1&city=%E9%83%91%E5%B7%9E').then((data) => {
+        return data.data
+    })
+  }
+
+  function send(wetaherData) {
+    return axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=103a6a5b-7d64-442a-86c9-07d2dc412c7c',{
+            msgtype: 'text',
+            text: {
+                'content': JSON.stringify(wetaherData, null, 4)
+            }
+        })
+  }
+/**
+ * 格式化一个时间字符串
+ * @param time
+ * @param format
+ * @returns {*}
+ */
+function timeString(time, format) {
+    const form = format ? format : 'YYYY-MM-DD HH:mm:ss';
+    return moment(time).format(form);
+}
+
+remind().then(res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err)
+})
